0f1d13430558217f380346d99736242c5731d54e
javi mena <menajavier2002@gmail.com>
2022-04-28 18:01:50 +0200

    añadiendo la ulrima funcion

---
+++ lib/firebase_storage/CloudStorageServices.dart
@@ -0,0 +1,4 @@
+import 'dart:html';
+
+import 'package:lions_film/firebase_storage/cloud_storage_result.dart';
+
--- lib/screens/shorts_screen.dart
+++ lib/screens/shorts_screen.dart
@@ -3,32 +3,74 @@ import 'package:flutter/material.dart';
 import 'package:lions_film/tiles/add_short_formulary.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
-class ShortsScreen extends StatelessWidget {
-  const ShortsScreen({Key? key}) : super(key: key);
+class ShortsScreen extends StatefulWidget {
 
   @override
+  State<ShortsScreen> createState() => _ShortsScreenState();
+}
+
+class _ShortsScreenState extends State<ShortsScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    final storageRef = FirebaseStorage.instance.ref('/file_picker');
+
+  }
+  @override
   Widget build(BuildContext context) {
+    // final storageRef = FirebaseStorage.instance.ref('/file_picker');
+    // final path = storageRef.fullPath;
+    // final name = storageRef.name;
     return Scaffold(
-      appBar: AppBar(),
-      floatingActionButton: FabCircularMenu(
-        ringColor: Colors.grey,
-        children: [
-          IconButton(icon: const Icon(Icons.add), onPressed: () {
-            Navigator.push(context,
-                MaterialPageRoute(builder: (context) => const AddShortFormulary()));
-          }),
-          IconButton(icon: const Icon(Icons.remove_red_eye), onPressed: () {
-            print('Ver mis cortos');
-          })
-        ],
-      ),
-    );
+        appBar: AppBar(),
+        floatingActionButton: FabCircularMenu(
+          ringColor: Colors.grey,
+          children: [
+            IconButton(
+                icon: const Icon(Icons.add),
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const AddShortFormulary()));
+                }),
+            IconButton(
+                icon: const Icon(Icons.remove_red_eye),
+                onPressed: () {
+                  print('Ver mis cortos');
+                })
+          ],
+        ),
+        body: FutureBuilder(
+          future: getShort(),
+          builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+            if (snapshot.hasData) {
+              final file = snapshot.data.items;
+              return GridView.count(crossAxisCount: 2,children: [
+                Card(
+                  child: Text(file.),
+                )
+              ],);
+            }
+          },
+
+        ));
   }
 }
 
-class FireStorageServices extends ChangeNotifier {
-  FireStorageServices();
-  static Future<dynamic> loadVideo(BuildContext context, String video) async {
-    return await FirebaseStorage.instance.ref().child(video).getDownloadURL();
-  }
+Future<Widget> getShort() async {
+  final storageRef = FirebaseStorage.instance.ref();
+  final spaceRef = storageRef.child("files/");
+  //propiedades
+  final path = spaceRef.fullPath;
+  final name = spaceRef.name;
+  return Column(
+    children: [
+       const Card(
+        color: Colors.red,
+      ),
+      Text(name)
+    ],
+  );
 }
